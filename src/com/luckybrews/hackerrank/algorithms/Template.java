ad11bf1d792d2be34450d6892e52ba02cc054d69
Marco Fiscato <marco@swiftkey.net>
2013-12-07 15:55:34 +0000

    Initial code drop

---
+++ src/com/luckybrews/hackerrank/algorithms/Template.java
@@ -0,0 +1,26 @@
+package com.luckybrews.hackerrank.algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Template {
+	
+	// SOLUTION
+	static int solution(int[] a,int k) {
+		return 0;
+	}
+	
+	@Test
+	public void test_01() {
+		
+		final int N = 0;
+		final int K = 0;
+		final int[] ARR = new int[] {1, 2};
+		final int EXPECTED = 0;
+		final int RESULT = solution(ARR, K);
+		
+		assertEquals(EXPECTED, RESULT);
+	}
+
+}
